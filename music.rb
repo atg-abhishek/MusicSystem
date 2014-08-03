a664dd7e6279e27dd9d188f0d7394a05dc214637
atg-abhishek <atg.abhishek@gmail.com>
2014-08-03 14:06:58 -0400

    added a third function that adds the ability to switch MIDI channels

--- music.rb
+++ music.rb
@@ -5,7 +5,22 @@ class LiveMIDI
 	OFF = 0x80
 	PC = 0xC0
 
-def initialize
-	open
+	def initialize
+		open
+	end
+
+	def note_on(channel, note, velocity=64)
+		message(ON | channel, note, velocity)
+	end
+
+	def note_off(channel, note, velocity=64)
+		message(OFF | channel, note, velocity)
+	end
+
+	def program_change(channel, preset)
+		message(PC | channel, preset)
+	end
+
 end
+
 
